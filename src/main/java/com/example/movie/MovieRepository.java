480e368730a5998c24c94c5ee7186a9d9514216a
yaseen shaik <yaseenshaik602@gmail.com>
2024-01-25 09:21:26 +0000

    solution

---
+++ src/main/java/com/example/movie/MovieRepository.java
@@ -0,0 +1,18 @@
+package com.example.movie;
+
+import com.example.movie.Movie;
+
+import java.util.*;
+
+public interface MovieRepository {
+
+    ArrayList<Movie> getMovies();
+
+    Movie getMovieById(int movieId);
+
+    Movie addMovie(Movie movie);
+
+    Movie updateMovie(int movieId, Movie movie);
+
+    void deleteMovie(int movieId);
+}
